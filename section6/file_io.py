95a187443965f983e24dbec4ff0e19c7909edc65
Stephen Hulme <shulme801@gmail.com>
2021-02-14 12:47:20 -0500

    14 Feb C

--- section6/file_io.py
+++ section6/file_io.py
@@ -1,12 +1,18 @@
-my_file  = open('/Users/shulme801/Dropbox/newCode/src/shulme801/python_cert/Section6/sample.txt')
+# my_file  = open('/Users/shulme801/Dropbox/newCode/src/shulme801/python_cert/Section6/sample.txt')
 
-content  = my_file.read()
-print(content)
+# content  = my_file.read()
+# print(content)
 
-print('-----------------------------\n')
-my_file.seek(0)
-data  = my_file.readlines()
+# print('-----------------------------\n')
+# my_file.seek(0)
+# data  = my_file.readlines()
+# my_file.close()
 
-for line in data:
-    print(line)
-print('-----------------------------\n')
+# for line in data:
+#     print(line)
+# print('\n-----------------------------\n')
+
+with open('/Users/shulme801/Dropbox/newCode/src/shulme801/python_cert/Section6/sample.txt') as my_file:
+    new_content = my_file.read()
+
+print(new_content)   
